a31161debda642a375dd9f9058a25f8166efb805
Kevin Pietruszka <kevinpiet13@gmail.com>
2021-08-29 19:56:33 -0400

    Switches to Selenium and copied the excel reading code from the other script

--- convo-script.py
+++ convo-script.py
@@ -1,14 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
 
+from typing import final
+import time
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
+browser = Chrome()
+browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
 
-URL = "https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-job_elements = soup.find_all("select", class_="ChoiceStructure Selection QR-QID36 QWatchTimer")
-building_select = soup.find(id="QR~QID36")
-test = soup.find()
-print(building_select)
-print(job_elements)
+try:
+    # 
+    element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID36~17")))
+    element.click()
+    time.sleep(10)
+finally:
+    browser.quit()
