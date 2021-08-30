cb980c5cb33807fa5689a72132d647e0863c6098
Kevin Pietruszka <kevinpiet13@gmail.com>
2021-08-30 19:32:38 -0400

    Adds functionality for several pages in the form

--- convo-script.py
+++ convo-script.py
@@ -13,39 +13,71 @@ browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
 
 
 def nextPage():
-    next_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "NextButton")))
+
+    next_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, "NextButton")))
     next_button.click()
 
 
-try:
 
-    # Select community
+def main():
 
-    # NAVW: QR~QID36~11
-    # NAVS: QR~QID36~12
-    # NAVE: QR~QID36~13
-    # BSH: QR~QID36~17
-    #TODO logic to decide building selection
-    element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID36~17")))
-    element.click()
+    try:
 
-    nextPage()
-    
-    # Find name of person
-    name = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID45")))
-    options = browser.find_elements_by_tag_name("option")
-    for opt in options:
-        #TODO replace with excel data
-        if opt.text == "Kevin Pietruszka":
-            opt.click()
+        time.sleep(2)
 
-    nextPage()
+        # Select community
+        # NAVW: QR~QID36~11
+        # NAVS: QR~QID36~12
+        # NAVE: QR~QID36~13
+        # BSH: QR~QID36~17
+        #TODO logic to decide building selection
+        area = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID36~17")))
+        area.click()
 
-    residents_name = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID2")))
+        nextPage()
+        
+        # Find name of person
+        name = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID45")))
+        options = browser.find_elements_by_tag_name("option")
+        for opt in options:
+            #TODO replace with excel data
+            if opt.text == "Kevin Pietruszka":
+                opt.click()
 
-    residents_name.send_keys("Heil Hydra")
+        #TODO Find better way to wait for next button. It has error occaisionaly 
+        time.sleep(0.5)
+        nextPage()
 
-    time.sleep(5)
+        # enter residents name
+        residents_name = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID2")))
+        residents_name.send_keys("Brian Youn")
 
-finally:
-    browser.quit()
+        #building
+        #TODO find ids for the other buildings
+        #BRN: QR~QID39~8, SMT: QR~QID39~9, HRS: QR~QID39~9
+        building = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID36~17")))
+        building.click()
+
+        time.sleep(0.5)
+        nextPage()
+
+        #floor
+        options = browser.find_elements_by_tag_name("option")
+        for opt in options:
+            #TODO replace with excel data
+            if opt.text == "1":
+                opt.click()
+
+        time.sleep(0.5)
+        nextPage()
+
+        #room number 
+        li = browser.find_elements_by_tag_name("li")
+
+        time.sleep(5)
+
+    finally:
+        browser.quit()
+
+if __name__ == "__main__":
+    main()
