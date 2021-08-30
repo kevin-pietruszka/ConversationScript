ca2ac6aa347c9f1ec65db6fb377ebabf45095d3a
Kevin Pietruszka <kevinpiet13@gmail.com>
2021-08-29 21:53:29 -0400

    Adds functionality for the next pages

--- convo-script.py
+++ convo-script.py
@@ -11,10 +11,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 browser = Chrome()
 browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
 
+
+def nextPage():
+    next_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "NextButton")))
+    next_button.click()
+
+
 try:
-    # 
+
+    # Select community
+
+    # NAVW: QR~QID36~11
+    # NAVS: QR~QID36~12
+    # NAVE: QR~QID36~13
+    # BSH: QR~QID36~17
+    #TODO logic to decide building selection
     element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID36~17")))
     element.click()
-    time.sleep(10)
+
+    nextPage()
+    
+    # Find name of person
+    name = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID45")))
+    options = browser.find_elements_by_tag_name("option")
+    for opt in options:
+        #TODO replace with excel data
+        if opt.text == "Kevin Pietruszka":
+            opt.click()
+
+    nextPage()
+
+    residents_name = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID2")))
+
+    residents_name.send_keys("Heil Hydra")
+
+    time.sleep(5)
+
 finally:
     browser.quit()
