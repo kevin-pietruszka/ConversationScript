99273584f876ac805126b2d660f5a6e54e24e82b
Kevin Pietruszka <kevinpiet13@gmail.com>
2021-08-30 22:43:42 -0400

    Carry on

--- convo-script.py
+++ convo-script.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 # Use RA_NAME, RA_EMAIL, 
-import excel_reader
+#import excel_reader
 
 browser = Chrome()
 browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
@@ -20,11 +20,6 @@ def nextPage():
     next_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, "NextButton")))
     next_button.click()
 
-<<<<<<< HEAD
-
-=======
-try:
->>>>>>> fe9ef5fceffa73b80f43e19a2ea058c37c47c5b8
 
 def main():
 
@@ -33,9 +28,9 @@ def main():
         time.sleep(2)
 
         # Select community
-        # NAVW: QR~QID36~11
-        # NAVS: QR~QID36~12
-        # NAVE: QR~QID36~13
+        # NAW: QR~QID36~11
+        # NAS: QR~QID36~12
+        # NAE: QR~QID36~13
         # BSH: QR~QID36~17
         #TODO logic to decide building selection
         area = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID36~17")))
@@ -62,32 +57,36 @@ def main():
         #building
         #TODO find ids for the other buildings
         #BRN: QR~QID39~8, SMT: QR~QID39~9, HRS: QR~QID39~9
-        building = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID36~17")))
-        building.click()
+        #TODO logic to decide building selection
+        building = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID39")))
+        options = browser.find_elements_by_tag_name("option")
+        for opt in options:
+            if opt.text == "BRN":
+                opt.click()
 
         time.sleep(0.5)
         nextPage()
-        # NAW: QR~QID36~11
-        # NAS: QR~QID36~12
-        # NAE: QR~QID36~13
-        # BSH: QR~QID36~17
-        #TODO logic to decide building selection
-        element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID36~17")))
-        element.click()
 
         #floor
+        building = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID58")))
         options = browser.find_elements_by_tag_name("option")
         for opt in options:
-            #TODO replace with excel data
             if opt.text == "1":
                 opt.click()
 
         time.sleep(0.5)
         nextPage()
 
-        #room number 
-        li = browser.find_elements_by_tag_name("li")
+        #room number
+        room_letter = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "QR~QID91")))
+        li = browser.find_elements_by_tag_name("label")
+        for l in li:
+            if l.text == "112":
+                l.click()
 
+
+        
+        
         time.sleep(5)
 
     finally:
