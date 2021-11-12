ad8218b106d426badf8710c678856ef2005e1039
Kevin Pietruszka <kevinpiet13@gmail.com>
2021-11-12 13:07:00 -0500

    Fixes for kantwon

--- convo-script.py
+++ convo-script.py
@@ -11,110 +11,107 @@ from selenium.webdriver.support.ui import WebDriverWait
 import excel_reader as er
 from ids import *
 
-browser = Chrome()
 
 def nextPage():
     #TODO Find better way to wait for next button. It has error occaisionaly
-    time.sleep(0.5)
+    time.sleep(1)
     next_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, "NextButton")))
     next_button.click()
+    time.sleep(1)
 
 def wait(id):
 
     return WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, id)))
     
-def parse_options(tag, key):
-
-    options = browser.find_elements_by_tag_name(tag)
-
-    for opt in options:
-        if opt.text == key:
-            opt.click()
-
+browser = Chrome()
 
 def main():
 
-    browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
+    # browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
+    browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_8bG7y2Ts0IHdXCK')
     time.sleep(3)
 
-    browser.maximize_window()
+    idx = 0
 
-    for resident in er.RESDIENTS:
-        print(resident)
-        #try:
-        
-        # Select community
-        area = wait(areas[resident["area"]])
-        area.click()
+    while True:
 
-        nextPage()
-        
-        #ra name
-        ra = wait(RAs[resident["ra_name"]])
-        ra.click()
+        if (idx >= len(er.RESDIENTS)):
+            break;
 
-        #ra email
-        ra_email = wait(ids["ra_email"])
-        if resident["ra_email"] == None:
-            pass
-        else:
-            ra_email.send_keys(resident["ra_email"])
+        try:
+            resident = er.RESDIENTS[idx]
+            print(resident)
+            #try:
+            
+            # Select community
+            area = wait(areas[resident["area"]])
+            area.click()
 
-        time.sleep(1)
-        nextPage()
-        time.sleep(1)
+            nextPage()
+            
+            #ra name
+            ra = wait(RAs[resident["ra_name"]])
+            ra.click()
 
-        #residents name
-        residents_name = wait(ids["resident"])
-        residents_name.send_keys(resident["name"])
+            #ra email
+            ra_email = wait(ids["ra_email"])
+            if resident["ra_email"] == None:
+                pass
+            else:
+                ra_email.send_keys(resident["ra_email"])
 
-        #building
-        b = wait(buildings[resident["building"]])
-        b.click()
+            nextPage()
 
-        time.sleep(1)
-        nextPage()
-        time.sleep(1)
+            #residents name
+            residents_name = wait(ids["resident"])
+            residents_name.send_keys(resident["name"])
 
-        #floor
-        f = wait(floors[resident["floor"]])
-        f.click()
+            #building
+            b = wait(buildings[resident["building"]])
+            b.click()
 
-        time.sleep(1)
-        nextPage()
-        time.sleep(1)
+            nextPage()
 
-        #room number and letter
-        time.sleep(3)
-        date = wait(ids["date"])
-        date.send_keys(resident["date"])
-        room = wait(rooms[resident["building"] + resident["apartment/room"]])
-        room.click()
-        letter = wait(letters[resident["bedroom"]])
-        letter.click()
+            #floor
+            f = wait(floors[resident["building"] + resident["floor"]])
+            f.click()
 
-        #contact type
-        # TODO
-        # parse_options("label", "In person")
-        inperson = wait("QID48-1-label")
-        inperson.click()
+            nextPage()
+            
 
-        #topic
-        # TODO
-        # parse_options("label", "Social/Get-to-know")
-        gettoknow = wait("QID41-1-label")
-        gettoknow.click()
+            #room number and letter
+            date = wait(ids["date"])
+            date.send_keys(resident["date"])
+            room = wait(rooms[resident["building"] + resident["apartment/room"]])
+            room.click()
+            letter = wait(letters[resident["bedroom"]])
+            letter.click()
 
-        time.sleep(1)
-        nextPage()
-        time.sleep(1)
+            #contact type
+            # TODO
+            # parse_options("label", "In person")
+            inperson = wait("QID48-1-label")
+            inperson.click()
 
-        desc = wait(ids["description"])
-        desc.send_keys(resident["description"])
+            #topic
+            # TODO
+            # parse_options("label", "Social/Get-to-know")
+            gettoknow = wait("QID41-1-label")
+            gettoknow.click()
 
-        time.sleep(1)
-        nextPage()
-        time.sleep(5)
+            nextPage()
+
+            desc = wait(ids["description"])
+            desc.send_keys(resident["description"])
+
+            # nextPage()
+            idx+=1
+            time.sleep(5)
+            
+        except:
+
+            browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_8bG7y2Ts0IHdXCK')
+            print("retrying")
 
 if __name__ == "__main__":
     main()
--- ids.py
+++ ids.py
@@ -12,30 +12,47 @@ ids = {
 }
 
 RAs = {
-
+    "Kantwon Rogers":"QR~QID127~105"
 }
 
 areas = {
     "NAW": "QR~QID36~11",
     "NAS": "QR~QID36~12",
     "NAE": "QR~QID36~13",
-    "BSH": "QR~QID36~17"
+    "BSH": "QR~QID36~17",
+    "HAN":"QR~QID36~17"
 }
 
 buildings = {
     "BRN": "QR~QID39~8",
     "SMT": "QR~QID39~9",
-    "HRS": "QR~QID39~10"
+    "HRS": "QR~QID39~10",
+    "HAN": "QR~QID39~11"
 }
 
 floors = {
     "BRN1": "QR~QID58~2",
     "BRN2": "QR~QID58~11",
     "BRN3": "QR~QID58~3",
+    "HAN2": "QR~QID115~11"
 }
 
 rooms = {
-
+    "HAN201":"QID88-1-label",
+    "HAN202":"QID88-160-label",
+    "HAN203":"QID88-161-label",
+    "HAN204":"QID88-162-label",
+    "HAN205":"QID88-163-label",
+    "HAN206":"QID88-164-label",
+    "HAN207":"QID88-165-label",
+    "HAN208":"QID88-166-label",
+    "HAN209":"QID88-167-label",
+    "HAN210":"QID88-168-label",
+    "HAN211":"QID88-169-label",
+    "HAN212":"QID88-170-label",
+    "HAN213":"QID88-171-label",
+    "HAN214":"QID88-172-label",
+    "HAN215":"QID88-173-label",
 }
 
 letters = {
