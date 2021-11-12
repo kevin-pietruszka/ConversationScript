176c64ebb4834798319933b4912447c87f4e4e6d
Kevin Pietruszka <kevinpiet13@gmail.com>
2021-11-12 12:33:51 -0500

    Plans for future improvements

--- convo-script.py
+++ convo-script.py
@@ -1,5 +1,3 @@
-
-import re
 from typing import final
 import time
 from selenium.webdriver import Chrome
@@ -9,21 +7,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-
-# Use RESIDENTS 
-
-import excel_reader
-
 # Use RA_NAME, RA_EMAIL, 
 import excel_reader as er
-
-from dictionary import *
+from ids import *
 
 browser = Chrome()
-
-
-
-
 
 def nextPage():
     #TODO Find better way to wait for next button. It has error occaisionaly
@@ -39,66 +27,12 @@ def parse_options(tag, key):
 
     options = browser.find_elements_by_tag_name(tag)
 
-import excel_reader as er
-
-browser = Chrome()
-
-
-ids = {
-    "NAW": "QR~QID36~11",
-    "NAS": "QR~QID36~12",
-    "NAE": "QR~QID36~13",
-    "BSH": "QR~QID36~17",
-    "ra":"QR~QID45",
-    "ra_email": "QR~QID38",
-    "resident":"QR~QID2",
-    "building":"QR~QID39",
-    "floor":"QR~QID95", #Kev had 58, 77 for NAE its different for each building SMT 95
-    "bedroom":"QR~QID91",
-    "date":"QR~QID3",
-    "description":"QR~QID49",
-    "calendar":"QID3_cal"
-}
-
-def detFloorID(building):
-    if building == "SMT":
-        return "QR~QID95"
-    elif building == "NAE":
-        return "QR~QID77"
-    elif building == "BRN":
-        return "QR~QID77"
-    else:
-        raise NotImplementedError
-
-
-def nextPage():
-    #TODO Find better way to wait for next button. It has error occaisionaly 
-    time.sleep(0.75)
-    next_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, "NextButton")))
-    next_button.click()
-    
-
-def wait(id):
-
-    return WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, id)))
-    
-def parse_options(tag, key):
-
-    options = browser.find_elements_by_tag_name(tag)
-
     for opt in options:
         if opt.text == key:
             opt.click()
-    
-    #print()
 
-failed = []
-fail = 0
 
 def main():
-
-    failed = []
-    fail = 0
 
     browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
     time.sleep(3)
@@ -109,23 +43,18 @@ def main():
         print(resident)
         #try:
         
-        browser.refresh()
-        time.sleep(5)
-        
-        
         # Select community
-        area = wait(ids[resident["area"]])
+        area = wait(areas[resident["area"]])
         area.click()
 
         nextPage()
         
         #ra name
-        wait(ids["ra"])
-        parse_options("option", resident["ra_name"])
+        ra = wait(RAs[resident["ra_name"]])
+        ra.click()
 
         #ra email
         ra_email = wait(ids["ra_email"])
-
         if resident["ra_email"] == None:
             pass
         else:
@@ -140,16 +69,16 @@ def main():
         residents_name.send_keys(resident["name"])
 
         #building
-        wait(ids["building"])
-        parse_options("option", resident["building"])
+        b = wait(buildings[resident["building"]])
+        b.click()
 
         time.sleep(1)
         nextPage()
         time.sleep(1)
 
         #floor
-        wait(detFloorID(resident["building"]))
-        parse_options("option", resident["floor"])
+        f = wait(floors[resident["floor"]])
+        f.click()
 
         time.sleep(1)
         nextPage()
@@ -157,24 +86,24 @@ def main():
 
         #room number and letter
         time.sleep(3)
-        wait(ids["bedroom"])
         date = wait(ids["date"])
-        wait("Logo")
-        wait(ids["calendar"])
-        parse_options("label", resident["apartment/room"])
-        parse_options("option", resident["bedroom"])
-
-
-        #date
         date.send_keys(resident["date"])
+        room = wait(rooms[resident["building"] + resident["apartment/room"]])
+        room.click()
+        letter = wait(letters[resident["bedroom"]])
+        letter.click()
 
         #contact type
         # TODO
-        parse_options("label", "In person")
+        # parse_options("label", "In person")
+        inperson = wait("QID48-1-label")
+        inperson.click()
 
         #topic
         # TODO
-        parse_options("label", "Social/Get-to-know")
+        # parse_options("label", "Social/Get-to-know")
+        gettoknow = wait("QID41-1-label")
+        gettoknow.click()
 
         time.sleep(1)
         nextPage()
@@ -185,23 +114,7 @@ def main():
 
         time.sleep(1)
         nextPage()
-        
-        
         time.sleep(5)
-        #browser.quit()
-        #time.sleep(5)
-
-            
-        """
-        except:
-            
-            failed.append(resident["name"])
-            fail+=1
-        """
-            
-    #print("This many entries failed, " + str(fail))
-    #print(failed)
-    #browser.quit()
 
 if __name__ == "__main__":
     main()
---
+++ ids.py
@@ -0,0 +1,48 @@
+ids = {
+    
+    "ra":"QR~QID45",
+    "ra_email": "QR~QID38",
+    "resident":"QR~QID2",
+    "building":"QR~QID39",
+    "floor":"QR~QID58", #TODO find al the floor values
+    "bedroom":"QR~QID91",
+    "date":"QR~QID3",
+    "description":"QR~QID49",
+    "calendar":"QID3_cal"
+}
+
+RAs = {
+
+}
+
+areas = {
+    "NAW": "QR~QID36~11",
+    "NAS": "QR~QID36~12",
+    "NAE": "QR~QID36~13",
+    "BSH": "QR~QID36~17"
+}
+
+buildings = {
+    "BRN": "QR~QID39~8",
+    "SMT": "QR~QID39~9",
+    "HRS": "QR~QID39~10"
+}
+
+floors = {
+    "BRN1": "QR~QID58~2",
+    "BRN2": "QR~QID58~11",
+    "BRN3": "QR~QID58~3",
+}
+
+rooms = {
+
+}
+
+letters = {
+    "A":"QR~QID91~1",
+    "B":"QR~QID91~2",
+    "C":"QR~QID91~3",
+    "D":"QR~QID91~4",
+    "E":"QR~QID91~5",
+    "F":"QR~QID91~6",
+}
