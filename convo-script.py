2c5f9fc22a75df618c8696b407cd6764140fe426
Brett Pietruszka <brettpietruszka@lawn-143-215-91-114.lawn.gatech.edu>
2022-01-20 11:44:15 -0500

    idek

--- convo-script.py
+++ convo-script.py
@@ -19,16 +19,32 @@ def nextPage():
     next_button.click()
     time.sleep(1)
 
+def backPage():
+    #TODO Find better way to wait for next button. It has error occaisionaly
+    time.sleep(1)
+    next_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, "PreviousButton")))
+    next_button.click()
+    time.sleep(1)
+
 def wait(id):
 
     return WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, id)))
+
+def clearTextBox(textBox):
+    if textBox == None:
+        return
+    try:
+        for i in range(200):
+            textBox.send_keys(Keys.BACK_SPACE)
+    except:
+        print("\nDelete TextBox Error (Text Box not None).\n")
     
 browser = Chrome()
 
 def main():
 
-    # browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
-    browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_8bG7y2Ts0IHdXCK')
+    browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
+    #browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_8bG7y2Ts0IHdXCK')
     time.sleep(3)
 
     idx = 0
@@ -36,81 +52,145 @@ def main():
     while True:
 
         if (idx >= len(er.RESDIENTS)):
-            break;
+            break
 
         try:
-            resident = er.RESDIENTS[idx]
-            print(resident)
-            #try:
-            
-            # Select community
-            area = wait(areas[resident["area"]])
-            area.click()
+
+            ########## Page 1 ##########
+            while True:
+                try:
+                    resident = er.RESDIENTS[idx]
+                    print(resident)
+                    #try:
+                    
+                    # Select community
+                    area = wait(areas[resident["area"]])
+                    area.click()
+                    break
+                except Exception as error:
+                    print(str(error))
+                    #browser.refresh()
+                    time.sleep(1)
 
             nextPage()
             
-            #ra name
-            ra = wait(RAs[resident["ra_name"]])
-            ra.click()
+            ########## Page 2 ##########
+            while True:
+                try:
+                    #ra name
+                    ra = wait(RAs[resident["ra_name"]])
+                    ra.click()
 
-            #ra email
-            ra_email = wait(ids["ra_email"])
-            if resident["ra_email"] == None:
-                pass
-            else:
-                ra_email.send_keys(resident["ra_email"])
+                    #ra email
+                    ra_email = wait(ids["ra_email"])
+                    if resident["ra_email"] == None:
+                        pass
+                    else:
+                        ra_email.send_keys(resident["ra_email"])
+                    break
+                except Exception as error:
+                    print(str(error))
+                    #browser.refresh()
+                    ra_email = wait(ids["ra_email"])
+                    clearTextBox(ra_email)
+                    time.sleep(1)
+
+
+            nextPage()
+
+            ########## Page 3 ##########
+            while True:
+                try:
+                    #residents name
+                    residents_name = wait(ids["resident"])
+                    residents_name.send_keys(resident["name"])
+
+                    #building
+                    b = wait(buildings[resident["building"]])
+                    b.click()
+                    break
+                except Exception as error:
+                    print(str(error))
+                    #browser.refresh()
+                    residents_name = wait(ids["resident"])
+                    clearTextBox(residents_name)
+                    time.sleep(1)
 
             nextPage()
 
-            #residents name
-            residents_name = wait(ids["resident"])
-            residents_name.send_keys(resident["name"])
-
-            #building
-            b = wait(buildings[resident["building"]])
-            b.click()
-
-            nextPage()
-
-            #floor
-            f = wait(floors[resident["building"] + resident["floor"]])
-            f.click()
+            ########## Page 4 ##########
+            while True:
+                try:
+                    #floor
+                    f = wait(floors[resident["building"] + resident["floor"]])
+                    f.click()
+                    break
+                except Exception as error:
+                    print(str(error))
+                    #browser.refresh()
+                    time.sleep(1)
 
             nextPage()
+
+            
+            ########## Page 5 ##########
+            while True:
+                try:
+                    #room number and letter
+                    date = wait(ids["date"])
+                    date.send_keys(resident["date"])
+                    room = wait(rooms[resident["building"] + resident["apartment/room"]])
+                    room.click()
+                    letter = wait(letters[resident["bedroom"]])
+                    letter.click()
+
+                    #contact type
+                    # TODO
+                    # parse_options("label", "In person")
+                    inperson = wait("QID48-1-label")
+                    inperson.click()
+
+                    #topic
+                    # TODO
+                    # parse_options("label", "Social/Get-to-know")
+                    gettoknow = wait("QID41-1-label")
+                    gettoknow.click()
+                    break
+                except Exception as error:
+                    print(str(error))
+                    date = wait(ids["date"])
+                    clearTextBox(date)
+                    #browser.refresh()
+                    time.sleep(1)
             
 
-            #room number and letter
-            date = wait(ids["date"])
-            date.send_keys(resident["date"])
-            room = wait(rooms[resident["building"] + resident["apartment/room"]])
-            room.click()
-            letter = wait(letters[resident["bedroom"]])
-            letter.click()
+            nextPage()
 
-            #contact type
-            # TODO
-            # parse_options("label", "In person")
-            inperson = wait("QID48-1-label")
-            inperson.click()
-
-            #topic
-            # TODO
-            # parse_options("label", "Social/Get-to-know")
-            gettoknow = wait("QID41-1-label")
-            gettoknow.click()
+            ########## Page 6 ##########
+            while True:
+                try:
+                    desc = wait(ids["description"])
+                    desc.send_keys(resident["description"])
+                    break
+                except Exception as error:
+                    print(str(error))
+                    #browser.refresh()
+                    desc = wait(ids["description"])
+                    clearTextBox(desc)
+                    time.sleep(1)
 
             nextPage()
 
-            desc = wait(ids["description"])
-            desc.send_keys(resident["description"])
 
-            # nextPage()
             idx+=1
             time.sleep(5)
             
         except:
 
-            browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_8bG7y2Ts0IHdXCK')
+            #browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_8bG7y2Ts0IHdXCK')
+            browser.get('https://gatech.co1.qualtrics.com/jfe/form/SV_da3BNVPrp4VvN5Q')
+            #browser.refresh()
+            time.sleep(0.20)
             print("retrying")
 
 if __name__ == "__main__":
--- ids.py
+++ ids.py
@@ -12,7 +12,8 @@ ids = {
 }
 
 RAs = {
-    "Kantwon Rogers":"QR~QID127~105"
+    "Kantwon Rogers":"QR~QID127~105",
+    "Brett Pietruszka": "QR~QID45~127"
 }
 
 areas = {
@@ -27,14 +28,16 @@ buildings = {
     "BRN": "QR~QID39~8",
     "SMT": "QR~QID39~9",
     "HRS": "QR~QID39~10",
-    "HAN": "QR~QID39~11"
+    "HAN": "QR~QID39~11",
+    "NAE": "QR~QID39~5"
 }
 
 floors = {
     "BRN1": "QR~QID58~2",
     "BRN2": "QR~QID58~11",
     "BRN3": "QR~QID58~3",
-    "HAN2": "QR~QID115~11"
+    "HAN2": "QR~QID115~11",
+    "NAE6": "QR~QID77~6"
 }
 
 rooms = {
@@ -53,7 +56,40 @@ rooms = {
     "HAN213":"QID88-171-label",
     "HAN214":"QID88-172-label",
     "HAN215":"QID88-173-label",
+    
+    "NAE602" : "QID83-1-label",
+    "NAE604" : "QID83-88-label",
+    "NAE605" : "QID83-89-label",
+    "NAE606" : "QID83-90-label",
+    "NAE607" : "QID83-91-label",
+    "NAE609" : "QID83-92-label",
+    "NAE610" : "QID83-93-label",
+    "NAE613" : "QID83-94-label",
+    "NAE614" : "QID83-95-label",
+    "NAE615" : "QID83-96-label",
+    "NAE616" : "QID83-97-label",
+    "NAE618" : "QID83-98-label",
+    "NAE619" : "QID83-99-label",
 }
+
+"""
+naerooms = {
+    "NAE602" : "QID83-1-label",
+    "NAE604" : "QID83-88-label",
+    "NAE605" : "QID83-89-label",
+    "NAE606" : "QID83-90-label",
+    "NAE607" : "QID83-91-label",
+    "NAE609" : "QID83-92-label",
+    "NAE610" : "QID83-93-label",
+    "NAE613" : "QID83-94-label",
+    "NAE614" : "QID83-95-label",
+    "NAE615" : "QID83-96-label",
+    "NAE616" : "QID83-97-label",
+    "NAE618" : "QID83-98-label",
+    "NAE619" : "QID83-99-label",
+}
+"""
+
 
 letters = {
     "A":"QR~QID91~1",
