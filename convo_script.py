156408ae2226d10df3c6d5bb902748e4dd700af6
Kevin Pietruszka <kevinpiet13@gmail.com>
2022-01-24 21:04:58 -0500

    Ignore

---
+++ convo_script.py
@@ -0,0 +1,124 @@
+# 3rd party libraries
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from chromedriver_py import binary_path 
+from service import Service
+import traceback
+
+
+#TODO  Change which link you are 
+is_nav = True
+
+# data files
+import excel_reader as er
+from nav_ids import *
+from general_ids import *
+
+
+# global vars and constants
+previous = None
+EXTIME = 3
+nav_link = 'https://gatech.co1.qualtrics.com/jfe/form/SV_4U8wGXJRFOMekfk' # NAV/BSH
+east_link = "https://gatech.co1.qualtrics.com/jfe/form/SV_72O9mThAOKPFYeq" # east campus
+
+link = None
+if is_nav:
+    link = nav_link
+else:
+    link = east_link
+
+def next_page():
+    next_button = WebDriverWait(browser, EXTIME).until(EC.element_to_be_clickable((By.NAME, "NextButton")))
+    next_button.click()
+    wait_for_page()
+
+def wait_for_page():
+
+    body = WebDriverWait(browser, EXTIME).until(EC.visibility_of_all_elements_located((By.ID, "SurveyEngineBody")))
+    if previous != None:
+        wait_stale()
+
+def wait_stale():
+    global previous
+    return WebDriverWait(browser, EXTIME).until(EC.staleness_of(previous))
+
+def wait_click(toClick):
+    global previous 
+    temp = WebDriverWait(browser, EXTIME).until(EC.element_to_be_clickable((By.ID, toClick)))
+    temp.click()
+    previous = temp
+
+def wait_type(toTypeTo, message):
+    global previous
+    temp = WebDriverWait(browser, EXTIME).until(EC.element_to_be_clickable((By.ID, toTypeTo)))
+    temp.send_keys(message)
+    previous = temp
+
+
+service_object = Service(binary_path)
+browser = webdriver.Chrome(executable_path=binary_path)
+
+def main():
+    
+    browser.get(link)
+
+    idx = 0
+
+    while True:
+
+        if (idx >= len(er.RESDIENTS)):
+            break
+
+        try:
+            wait_for_page()
+            resident = er.RESDIENTS[idx]
+            print(resident)
+
+            area = wait_click(areas[resident['area']])
+            next_page()
+
+            ra = wait_click(ras[resident['ra_name']])
+
+            if is_nav:
+                next_page()
+
+            res_name = wait_type(ids['resident'], resident['name'])
+            b = wait_click(buildings[resident['building']])
+            next_page()
+
+            f = wait_click(floors[resident['building'] + resident['floor']])
+            next_page()
+
+            
+            r = wait_click(rooms[resident['building'] + resident['apartment/room']])
+            l = wait_click(letters[resident['bedroom']])
+            d = wait_type(ids['date'], resident['date'])
+
+            meth = wait_click(methods[str(resident['method'])])
+
+            topic = wait_click(topics[str(resident['topic'])])
+
+            next_page()
+
+            if resident['topic'] == 7: 
+                purpose = wait_click(purposes[str(resident['purpose'])])
+
+            desc = wait_type(ids['description'], resident['description'])
+            
+            # next_page()
+
+            idx+=1
+            
+        except:
+
+            print(traceback.format_exc())
+            browser.delete_all_cookies()
+            browser.get(link)
+            print("Retrying")
+            browser.quit()
+
+if __name__ == "__main__":
+    main()
+    browser.quit()
---
+++ general_ids.py
@@ -0,0 +1,49 @@
+ids = {
+    
+    "ra":"QR~QID45",
+    "ra_email": "QR~QID38",
+    "resident":"QR~QID2",
+    "building":"QR~QID39",
+    "floor":"QR~QID58",
+    "bedroom":"QR~QID91",
+    "date":"QR~QID3",
+    "description":"QR~QID49",
+    "calendar":"QID3_cal"
+}
+
+letters = {
+    "A":"QR~QID91~1",
+    "B":"QR~QID91~2",
+    "C":"QR~QID91~3",
+    "D":"QR~QID91~4",
+    "E":"QR~QID91~5",
+    "F":"QR~QID91~6",
+}
+
+methods = {
+    "1":"QID48-1-label", # In person
+    "2":"QID48-2-label", # Video call
+    "3":"QID48-3-label", # Phone Call
+    "4":"QID48-4-label", # Text/Messaging
+    "5":"QID48-5-label", # In your residence hall
+    "6":"QID48-6-label", # Around campus
+}
+
+topics = {
+    "1":"QID41-1-label", # Social/Get-to-know
+    "2":"QID41-2-label", # Academic Issues & Successes
+    "3":"QID41-3-label", # Campus Involvement/Engagement
+    "4":"QID41-4-label", # Roommate & Community dynamics
+    "5":"QID41-5-label", # Current Topics & Events
+    "6":"QID41-6-label", # Crisis & Mental Health Concern
+    "7":"QID41-8-label", # Intentional Conversation Theme
+}
+
+purposes = {
+    "1":"QID109-1-label", # Transition
+    "2":"QID109-2-label", # Social Connection
+    "3":"QID109-3-label", # Academics
+    "4":"QID109-4-label", # Health and Wellbeing
+    "5":"QID109-5-label", # Goals/Career
+    "6":"QID109-6-label", # Closure
+}
